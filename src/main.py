5acb447c28188e83b7719126e91e5802df1ecf27
Tahir Mehmood <38874540+tahir-mm@users.noreply.github.com>
2025-04-11 22:21:21 +1000

    Update main.py
    executing first query

--- src/main.py
+++ src/main.py
@@ -23,11 +23,17 @@ def main(context):
         # Log messages and errors to the Appwrite Console
         # These logs won't be seen by your end users
         context.log("Total users: " + str(response["total"]))
-        queries = [{'limit': 100}]
+        # queries = [{'limit': 100}]
         result = databases.list_documents(
             database_id=os.environ["DATABASE_ID"],
             collection_id=os.environ["COLLECTION_ID"],
-            queries
+            queries = {
+            
+                "$filter": "order_status='Completed'",  # Replace with your actual field name and value
+                # "$filter": "bmonth=11",  # Replace with your actual field name and value
+                "$limit": 500  # Replace with the desired limit
+                # "$offset": 0  # Replace with the desired offset
+            }
         )
 
         return context.res.json({
