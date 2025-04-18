4d1adc65cf432aba99b36d68f51a133850deb6f6
Tahir Mehmood <tahirrajput@gmail.com>
2025-04-18 16:24:35 +1000

    removed variable

--- src/main.py
+++ src/main.py
@@ -277,7 +277,7 @@ def main(context):
     # For this example, we're using the Users service
     client = (
         Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
+        # .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
         .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
         .set_key(context.req.headers["x-appwrite-key"])
     )
